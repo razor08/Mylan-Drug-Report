c366e3fa3955d27a89f7aa8f6a3adf54343b3fc1
razor08 <razor.sinha.08@gmail.com>
2018-10-14 09:32:54 +0530

    [Fix] Auth Fix

--- config/auth.js
+++ config/auth.js
@@ -1,21 +1,21 @@
-module.exports = {
-    'googleAuth': {
-        'clientID': '650880893955-7u5q66scf45coo78cv6kmaa7tdh638f0.apps.googleusercontent.com',
-        'clientSecret': 'Vl22DgjgXbV0uwl31R9JQQWh',
-        'callbackURL': 'http://localhost:3000/auth/google/callback'
-    },
-    'sendgrid': {
-        'apiKey': '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'
-    }
-}
-
 // module.exports = {
 //     'googleAuth': {
 //         'clientID': '650880893955-7u5q66scf45coo78cv6kmaa7tdh638f0.apps.googleusercontent.com',
 //         'clientSecret': 'Vl22DgjgXbV0uwl31R9JQQWh',
-//         'callbackURL': 'https://mylandrug.ml/auth/google/callback'
+//         'callbackURL': 'http://localhost:3000/auth/google/callback'
 //     },
 //     'sendgrid': {
 //         'apiKey': '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'
 //     }
 // }
+
+module.exports = {
+    'googleAuth': {
+        'clientID': '650880893955-7u5q66scf45coo78cv6kmaa7tdh638f0.apps.googleusercontent.com',
+        'clientSecret': 'Vl22DgjgXbV0uwl31R9JQQWh',
+        'callbackURL': 'https://mylandrug.ml/auth/google/callback'
+    },
+    'sendgrid': {
+        'apiKey': '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'
+    }
+}
